584853c0463e3cae8f496170f3fda21804ec409c
PedroSehn <pedrorsehn@hotmail.com>
2023-03-22 16:24:11 -0300

    reinicia desenvolvimento do projeto

--- portifolio_2023/src/components/header/index.js
+++ portifolio_2023/src/components/header/index.js
@@ -1,17 +1,11 @@
 import './style.scss';
-import headerIcons from '../../assets/header'
-import HeaderIcon from './header-icons';
 
 
 function Header() {
-  const icons = Object.keys(headerIcons);
 
   return (
     <header className="Header">
-       { icons.map((index) => {
-          const icon = headerIcons[index];
-          return <HeaderIcon icons={ icon } name={index} refer={`${index}-section`}/>
-       }) }
+       
     </header>
   );
 }
